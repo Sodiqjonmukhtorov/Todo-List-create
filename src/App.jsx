831a870fd97af161fb59fe9005f25f171a6d0d6b
sodiqjon202 <sodiqjonm16@gmail.com>
2024-06-25 10:24:21 +0300

    Liber

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className='text-blue-400'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt doloribus, illo porro nobis magnam, sit corporis vitae, consequuntur officia repudiandae veritatis odit blanditiis optio! Accusantium cupiditate iure totam incidunt optio?</p>
+    </>
+  )
+}
+
+export default App
